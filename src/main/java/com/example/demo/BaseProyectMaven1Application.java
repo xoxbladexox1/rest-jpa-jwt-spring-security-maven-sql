768726d5cfa58d028abe13c35615cf9f35c0c010
Moix <Moix95@gmail.com>
2020-02-22 18:57:13 -0500

    first commit

---
+++ src/main/java/com/example/demo/BaseProyectMaven1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseProyectMaven1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseProyectMaven1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/dao/UserDaoImpl.java
@@ -0,0 +1,9 @@
+package com.example.demo.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.DAOUser;
+
